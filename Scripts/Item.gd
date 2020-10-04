35876bf0e7fca41140ba4dc80ebcfb6acd171d36
Abhinav Kuruvila <abhinav.kuruvila.joseph@gmail.com>
2020-10-04 10:30:02 +0530

    Working on Store

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -3,5 +3,5 @@ extends Button
 export var type : String
 
 func _on_Button_pressed():
-	get_node("../../../Popups/"+type+"buy").show()
 	get_node("../../../Popups").show()
+	get_node("../../../Popups/"+type+"buy").popup()
